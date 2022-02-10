640a129d1f4605b78549214c8a0682e4208a7e21
TKForgeron <trollololollolol.js@gmail.com>
2022-02-10 17:05:51 +0100

    project setup

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<title>Generate Schedule</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<script src="https://use.fontawesome.com/3397298754.js"></script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+.bgimg {
+  background-image: url('img/sawa.jpg');
+  min-height: 100%; 
+  background-position: center;
+  background-size: cover;
+}
+</style>
+<body>
+
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+  <div class="w3-display-topleft w3-padding-large w3-xlarge">
+    gwn_doen
+  </div>
+  <div class="w3-display-middle">
+    <b><h1 class="w3-jumbo w3-center w3-animate-top">BEGIN HIER</h1></b>
+    <hr class="w3-border-white" style="margin:auto;width:50%">
+    <p class="w3-large w3-black w3-text-white w3-center">
+      <?php
+      echo "AAP 3"
+      ?>
+    </p>
+  </div>
+  <div class="w3-display-bottomleft w3-padding-large">
+    Go to <a href="https://github.com/TKForgeron/AnimalObservationRandomizer">source code</a>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ requirements.md
@@ -0,0 +1,24 @@
+13 dieren
+dag wordt verdeeld in 4 delen
+gelijk aantal observatieuren, en gelijk verdeeld over dagdelen
+per dagdeel 3 observaties mogelijk (observatie duurt 20min)
+4 dagen in de week, 10 weken
+
+
+int time_os
+int time_observed
+
+IF time_os > 2min AND time_observed > 2min
+	THEN houdt time_observed bij en ga naar volgend dier
+ELSEIF time_os > 2min AND time_observed < 2min
+	THEN gooi de hele observatie weg
+ELSE houdt time_observed bij en ga door met je observatie
+
+
+planning met bovenstaande vereisten
+counter per dier met aantal minuten
+dropdown met lijstje dieren
+
+DEADLINE: 
+	DONDERDAG 17 FEB (alleen schema)
+	DINSDAG 1 MAART (complete ding)
